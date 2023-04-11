681ef5b119b7193cc86a3efbd15a0f5450d25d25
hugo morceau <27mhugo@gmail.com>
2023-04-11 10:20:02 +0200

    add AuditableEntity on users

--- back/src/module/users/user.entity.ts
+++ back/src/module/users/user.entity.ts
@@ -1,10 +1,11 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Celebrity } from 'src/module/celebrities/entities/celebrity.entity';
+import { AuditableEntity } from '../shared/entities/auditable.entity';
 
 @Entity({ name: 'users' })
 @ObjectType()
-export class User {
+export class User extends AuditableEntity {
   @Field(() => Int) // GraphQL
   @PrimaryGeneratedColumn() // TypeORM
   id: number;
