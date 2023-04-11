3c141a4ba66d7902e1349f4ba2d46f435760d39d
hugo morceau <27mhugo@gmail.com>
2023-04-11 10:16:04 +0200

    predictions_evidences relation with evidence

--- back/src/module/evidences/entities/evidence.entity.ts
+++ back/src/module/evidences/entities/evidence.entity.ts
@@ -1,7 +1,20 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { PredictionEvidence } from 'src/module/predictions-evidences/entities/prediction-evidence.entity';
+import { AuditableEntity } from 'src/module/shared/entities/auditable.entity';
+import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'evidences' })
 @ObjectType()
-export class Evidence {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class Evidence extends AuditableEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // Evidence 0,n - 0,1 PredictionEvidence
+  @OneToMany(
+    () => PredictionEvidence,
+    (predictionEvidences) => predictionEvidences.evidence,
+  )
+  @Field(() => [PredictionEvidence], { nullable: true })
+  predictionEvidences: PredictionEvidence[];
 }
--- back/src/module/predictions-evidences/entities/prediction-evidence.entity.ts
+++ back/src/module/predictions-evidences/entities/prediction-evidence.entity.ts
@@ -1,4 +1,5 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Evidence } from 'src/module/evidences/entities/evidence.entity';
 import { Prediction } from 'src/module/predictions/entities/prediction.entity';
 import { AuditableEntity } from 'src/module/shared/entities/auditable.entity';
 import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
@@ -6,7 +7,7 @@ import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 @Entity({ name: 'predictions_evidences' })
 @ObjectType()
 export class PredictionEvidence extends AuditableEntity {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -20,4 +21,9 @@ export class PredictionEvidence extends AuditableEntity {
   prediction: Prediction;
 
   // predictions_evidences 1,1 - 0,n evidences
+  @ManyToOne(() => Evidence, (evidence) => evidence.predictionEvidences, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  evidence: Evidence;
 }
