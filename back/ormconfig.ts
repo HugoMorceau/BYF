c6349cb56d9a573a2b7fbaf6ce49144f5cf4fc18
Hugo Morceau <106115253+HugoMorceau@users.noreply.github.com>
2024-02-29 15:47:52 +0100

    fix nest typeorm connection do database when in docker (added dotenv in ormconfig.ts)

--- back/ormconfig.ts
+++ back/ormconfig.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
+dotenv.config({ path: './src/config/.env' });
+console.log('host ormconfig : ' + process.env.DATABASE_HOST);
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST || 'localhost',
+  // host: 'byf-db',
   port: 5432,
   username: 'byf',
   password: 'byf',
--- back/src/main.ts
+++ back/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import * as cors from 'cors';
 import { AppModule } from './app.module';
 
-console.log('host : ' + process.env.DATABASE_HOST);
+console.log('hostt : ' + process.env.DATABASE_HOST);
 
 async function bootstrap() {
+  console.log('Starting the application bootstrap...');
   const app = await NestFactory.create(AppModule);
+  console.log('Application created, adding cors...');
   app.use(cors());
-  await app.listen(3100);
+  await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log('Environment: ' + process.env.NODE_ENV);
   console.log('USER ' + process.env.DATABASE_USER);
